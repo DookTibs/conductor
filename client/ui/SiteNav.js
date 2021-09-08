f1a5724e7dea7116cf61391e4e5d29309f0b17a7
Tom Feiler <tfeiler@gmail.com>
2021-09-07 23:41:16 -0500

    starting to add bottom nav bar, for eventual status display

--- client/ui/SiteNav.js
+++ client/ui/SiteNav.js
@@ -8,3 +8,20 @@ Template.siteTopNavBar.helpers({
 		return Session.get("CONTEXT_ID")
 	}
 });
+
+Template.siteBottomNavBar.onRendered(function x() {
+	$("#expander a").click(function() {
+		console.log("expand...");
+		var bottomNavDiv = $("div.bottom_nav");
+		if (bottomNavDiv.hasClass("expanded")) {
+			bottomNavDiv.removeClass("expanded");
+			$("div#__blaze-root").removeClass("shrunk");
+			$(this).html("EXPAND");
+		} else {
+			bottomNavDiv.addClass("expanded");
+			$("div#__blaze-root").addClass("shrunk");
+			$(this).html("SHRINK");
+		}
+
+	});
+});
